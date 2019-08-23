829d0cc29ff2f2baf34b92c550f340b2022f6918
AceIsHuman <menoman1995@yahoo.com>
2019-08-22 22:41:15 -0500

    create register form

--- src/components/user/Register.js
+++ src/components/user/Register.js
@@ -1,9 +1,37 @@
 import React from 'react';
+import { Grid, Header, Button } from 'semantic-ui-react';
+import { Form, Field, withFormik } from 'formik';
+
 
 const Register = () => {
     return (
-        <div>Inside of Register</div>
+        <Grid textAlign='center' style={{height: '100vh'}} verticalAlign='middle'>
+            <Grid.Column>
+                <Header as='h2' textAlign='center'>
+                    Welcome to Wunderlist 2.0!
+                </Header>
+                <Form>
+                    <div>
+                        <Field type='name' name='firstName' placeholder='First Name' />
+                    </div>
+
+                    <div>
+                        <Field type='name' name='lastName' placeholder='Last Name' />
+                    </div>
+                    <div>
+                        <Field type='username' name='username' placeholder='Username' />
+                    </div>
+
+                    <div>
+                        <Field type='password' name='password' placeholder='Password' />
+                    </div>
+                    <Button>Create New Account</Button>
+                </Form>
+            </Grid.Column>
+        </Grid>
     )
 }
 
-export default Register;
+const formikForm = withFormik({})(Register);
+
+export default formikForm;
