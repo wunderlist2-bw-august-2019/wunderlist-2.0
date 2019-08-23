d33eb4bc483e13b3d028cba0909b079c7d9db4ea
AceIsHuman <menoman1995@yahoo.com>
2019-08-22 23:47:22 -0500

    add login link

--- src/components/user/Register.js
+++ src/components/user/Register.js
@@ -1,32 +1,38 @@
 import React from 'react';
-import { Grid, Header, Button } from 'semantic-ui-react';
+import { Grid, Segment, Header, Button, Message } from 'semantic-ui-react';
 import { Form, Field, withFormik } from 'formik';
+import { Link } from 'react-router-dom';
 
 
 const Register = () => {
     return (
         <Grid textAlign='center' style={{height: '100vh'}} verticalAlign='middle'>
-            <Grid.Column>
+            <Grid.Column style={{maxWidth: 500}}>
                 <Header as='h2' textAlign='center'>
                     Welcome to Wunderlist 2.0!
                 </Header>
                 <Form>
-                    <div>
-                        <Field type='name' name='firstName' placeholder='First Name' />
-                    </div>
+                    <Segment stacked>
+                        <div>
+                            <Field type='name' name='firstName' placeholder='First Name' />
+                        </div>
 
-                    <div>
-                        <Field type='name' name='lastName' placeholder='Last Name' />
-                    </div>
-                    <div>
-                        <Field type='username' name='username' placeholder='Username' />
-                    </div>
+                        <div>
+                            <Field type='name' name='lastName' placeholder='Last Name' />
+                        </div>
+                        <div>
+                            <Field type='username' name='username' placeholder='Username' />
+                        </div>
 
-                    <div>
-                        <Field type='password' name='password' placeholder='Password' />
-                    </div>
-                    <Button>Create New Account</Button>
+                        <div>
+                            <Field type='password' name='password' placeholder='Password' />
+                        </div>
+                        <Button>Create New Account</Button>
+                    </Segment>
                 </Form>
+                <Message>
+                    <Link to='/'>Log In</Link>
+                </Message>
             </Grid.Column>
         </Grid>
     )
