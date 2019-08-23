bc847db2a95e2049eb792d7052babc7a464e0bb1
AceIsHuman <menoman1995@yahoo.com>
2019-08-22 22:34:47 -0500

    create login form

--- src/components/user/Login.js
+++ src/components/user/Login.js
@@ -1,9 +1,30 @@
 import React from 'react';
+import { Grid, Header, Button } from 'semantic-ui-react';
+import { Form, Field, withFormik } from 'formik';
+
 
 const Login = () => {
     return (
-        <div>Inside of Login</div>
+        <Grid textAlign='center' style={{height: '100vh'}} verticalAlign='middle'>
+            <Grid.Column>
+                <Header as='h2' textAlign='center'>
+                    Welcome to Wunderlist 2.0!
+                </Header>
+                <Form>
+                    <div>
+                        <Field type='username' name='username' placeholder='Username' />
+                    </div>
+
+                    <div>
+                        <Field type='password' name='password' placeholder='Password' />
+                    </div>
+                    <Button>Login</Button>
+                </Form>
+            </Grid.Column>
+        </Grid>
     )
 }
 
-export default Login;
+const formikForm = withFormik({})(Login);
+
+export default formikForm;
