703201e4c6489c7d9dbf91e19a84d44ed20ffc2a
AceIsHuman <menoman1995@yahoo.com>
2019-08-23 21:57:18 -0500

    create token state

--- src/App.js
+++ src/App.js
@@ -1,13 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.css';
 import AppRouter from './components/AppRouter';
 
 function App() {
+  const [token, setToken] = useCustomHook(localStorage.getItem("token"));
+
   return (
     <div className="App">
       <AppRouter />
     </div>
   );
+
+  function useCustomHook(name) {
+    const [storage, setStorage] = useState(localStorage.getItem(name));
+  
+    const placeInStorage = value => {
+        if (!value) {
+            localStorage.removeItem(name);
+            setStorage();
+        } else {
+            localStorage.setItem(name, value);
+            setStorage(value);
+        }
+    };
+
+    return [storage, placeInStorage];
+  }
 }
 
 export default App;
