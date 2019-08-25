bc7fd3e93e35b7d63c79c2eac4a739d098e60b6d
gaetanodaidone <gaetanodaidone@gmail.com>
2019-08-25 09:28:42 -0400

    todo layout

---
+++ src/components/tasks/Layout.js
@@ -0,0 +1,14 @@
+import React, { memo } from "react";
+import { AppBar, Toolbar, Typography, Paper } from "@material-ui/core";
+
+const Layout = memo(props => (
+  <Paper
+    elevation={0}
+    style={{ padding: 0, margin: 0, backgroundColor: "#fafafa" }}
+  >
+
+    {props.children}
+  </Paper>
+));
+
+export default Layout;
