1b79ba424080e06d0924a8ec0a0f02f9d9850563
gaetanodaidone <gaetanodaidone@gmail.com>
2019-08-25 09:30:13 -0400

    todo, basic todo is added. components of the list need to be added to the home page and the add to do needs to be seperate

---
+++ src/components/tasks/Todo.js
@@ -0,0 +1,39 @@
+
+
+import React, { memo } from "react";
+import ReactDOM from "react-dom";
+
+import { useInputValue, useTodos } from "../Hooks/custom-hooks";
+
+import Layout from "./Layout";
+
+import AddTodo from "./AddTodo";
+import TodoList from "./TodoList";
+
+const TodoApp = memo(props => {
+  const { inputValue, changeInput, clearInput, keyInput } = useInputValue();
+  const { todos, addTodo, checkTodo, removeTodo } = useTodos();
+
+  const clearInputAndAddTodo = _ => {
+    clearInput();
+    addTodo(inputValue);
+  };
+
+  return (
+    <Layout>
+      <AddTodo
+        inputValue={inputValue}
+        onInputChange={changeInput}
+        onButtonClick={clearInputAndAddTodo}
+        onInputKeyPress={event => keyInput(event, clearInputAndAddTodo)}
+      />
+      <TodoList
+        items={todos}
+        onItemCheck={idx => checkTodo(idx)}
+        onItemRemove={idx => removeTodo(idx)}
+      />
+    </Layout>
+  );
+});
+
+export default TodoApp;
