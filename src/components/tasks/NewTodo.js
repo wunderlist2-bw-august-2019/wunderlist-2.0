1e70afc043ee3d36497190fa542b8a22dc1eec01
AceIsHuman <menoman1995@yahoo.com>
2019-08-27 22:35:47 -0500

    create form for new task

---
+++ src/components/tasks/NewTodo.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Form, Field, withFormik } from 'formik';
+
+const NewTask = props => {
+  return (
+    <Form className="ui form">
+      <div className="equal width fields">
+        <div className="field">
+          <div className="ui fluid input">
+            <Field className="ui fluid input" type="input" label="Task" name="task" placeholder="Add Todo Here" />
+          </div>
+        </div>
+        <div className="field">
+          <div className="ui fluid input">
+            <Field type="input" label="Category" name="title" placeholder="Category" />
+          </div>
+        </div>
+        <div className="field">
+          <div className="ui fluid input">
+            <Field type="input" label="Due Date" name="setDate" placeholder="Due Date" />
+          </div>
+        </div>
+      </div>
+      <div className="field">
+        <Field component="textarea" label="Notes" name="notes" placeholder="Notes" rows="2" />
+      </div>
+    </Form>
+  )
+}
+
+const formikForm = withFormik({})(NewTask);
+
+export default formikForm;
