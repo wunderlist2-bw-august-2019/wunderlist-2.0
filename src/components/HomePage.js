91da543e1136087b85b8c77f043b606932c793ac
AceIsHuman <menoman1995@yahoo.com>
2019-08-27 23:23:02 -0500

    fix indent

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,18 +1,4 @@
 import React, { useState, memo } from 'react';
-import PropTypes from 'prop-types';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import Typography from '@material-ui/core/Typography';
-import InputBase from '@material-ui/core/InputBase';
-import { fade } from '@material-ui/core/styles/colorManipulator';
-import { withStyles } from '@material-ui/core/styles';
-import MenuIcon from '@material-ui/icons/Menu';
-import SearchIcon from '@material-ui/icons/Search';
-import TodoList from './tasks/TodoList';
-import AddTodo from './tasks/AddTodo';
-import Layout from './tasks/Layout';
-import { useInputValue , useTodos } from './Hooks/CustomHooks';
 import Todo from './tasks/Todo';
 import SearchBar from './Menu';
 import  '../index.css';
@@ -20,9 +6,9 @@ import  '../index.css';
 function HomePage(props) {
   return (
     <div>
-  <SearchBar />
-  <Todo />
-  </div>
+      <SearchBar />
+      <Todo />
+    </div>
   )
 }
 
