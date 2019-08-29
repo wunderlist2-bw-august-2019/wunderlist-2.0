5a1776920eb7efa7c1dadf407e5b3c78c0a33fe1
gaetanodaidone <gaetanodaidone@gmail.com>
2019-08-29 14:02:39 -0400

    added modal

---
+++ src/components/tasks/Modal.js
@@ -0,0 +1,65 @@
+import React from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Modal from '@material-ui/core/Modal';
+import Backdrop from '@material-ui/core/Backdrop';
+import Fade from '@material-ui/core/Fade';
+import NewTodo from './tasks/NewTodo';
+import AddIcon from '@material-ui/icons/Add';
+import Fab from '@material-ui/core/Fab';
+
+const useStyles = makeStyles(theme => ({
+    modal: {
+      display: 'flex',
+      alignItems: 'center',
+      justifyContent: 'center',
+    },
+    paper: {
+      backgroundColor: theme.palette.background.paper,
+      border: '2px solid #000',
+      boxShadow: theme.shadows[5],
+      padding: theme.spacing(5,10, 10),
+      width: 1000,
+      height: 300,
+    },
+  }));
+  
+  export default function TransitionsModal() {
+    const classes = useStyles();
+    const [open, setOpen] = React.useState(false);
+  
+    const handleOpen = () => {
+      setOpen(true);
+    };
+  
+    const handleClose = () => {
+      setOpen(false);
+    };
+  
+    return (
+      <div>
+      <Fab size="small" color="primary" aria-label="add" className={classes.margin} onClick={handleOpen}>
+      <AddIcon />
+    </Fab>
+
+        <Modal
+          aria-labelledby="transition-modal-title"
+          aria-describedby="transition-modal-description"
+          className={classes.modal}
+          open={open}
+          onClose={handleClose}
+          closeAfterTransition
+          BackdropComponent={Backdrop}
+          BackdropProps={{
+            timeout: 500,
+          }}
+        >
+          <Fade in={open}>
+            <div className={classes.paper}>
+          <NewTodo />
+            </div>
+          </Fade>
+        </Modal>
+      </div>
+    );
+  }
+  
