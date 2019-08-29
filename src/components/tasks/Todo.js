1cd3557dc5b654a7aadeeb0a9ace1e53916f56e1
AceIsHuman <menoman1995@yahoo.com>
2019-08-28 20:35:23 -0500

    create todo state and use api call to fill data

--- src/components/tasks/Todo.js
+++ src/components/tasks/Todo.js
@@ -1,5 +1,5 @@
-import React, { memo } from "react";
-import ReactDOM from "react-dom";
+import React, { useState, memo, useEffect } from "react";
+import axios from 'axios';
 import { useInputValue, useTodos } from "../Hooks/CustomHooks";
 import Layout from "./Layout";
 import NewTodo from "./NewTodo";
@@ -7,24 +7,37 @@ import TodoList from "./TodoList";
 import "./todoList.css";
 
 const TodoApp = memo(props => {
-  const { inputValue, changeInput, clearInput, keyInput } = useInputValue();
-  const { todos, addTodo, checkTodo, removeTodo } = useTodos();
+  // const { inputValue, changeInput, clearInput, keyInput } = useInputValue();
+  // const { todos, addTodo, checkTodo, removeTodo } = useTodos();
+
+  // const clearInputAndAddTodo = _ => {
+  //   clearInput();
+  //   addTodo(inputValue);
+  // };
+  // let todoCount = todos.length;
+
+  // if (todoCount = 0) {
+  //   return (
+  //     <div className= "todoList">
+  //       <h3>Are you being lazy or productive? we ask because you have nothing to do. </h3>
+  //     </div>
+  //   )
+  // }
+
+  const [todos, setTodos] = useState([]);
+  
+  const getTodos = () => {
+    axios.get('https://wunderlist-2.herokuapp.com/api/todos/')
+      .then(res => {
+        setTodos(res.data.filter(todo => `${todo.user_id}` === localStorage.getItem("user_id")));
+      })
+  }
+
+  useEffect(() => {
+    getTodos();
+  }, [])
 
 
-  const clearInputAndAddTodo = _ => {
-    clearInput();
-    addTodo(inputValue);
-  };
-
-  let todoCount = todos.length;
-
-// if (todoCount = 0) {
-//   return (
-//     <div className= "todoList">
-//       <h3>Are you being lazy or productive? we ask because you have nothing to do. </h3>
-//     </div>
-//   )
-// }
 return (      
 
     <Layout>
@@ -32,8 +45,8 @@ return (
 <div className="todoList">
       <TodoList
         items={todos}
-        onItemCheck={idx => checkTodo(idx)}
-        onItemRemove={idx => removeTodo(idx)}
+        // onItemCheck={idx => checkTodo(idx)}
+        // onItemRemove={idx => removeTodo(idx)}
       />
       </div>
     </Layout>
